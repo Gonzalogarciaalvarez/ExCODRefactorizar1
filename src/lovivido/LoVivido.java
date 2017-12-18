b657678fdab549fae2a5100989c41dde9335afe9
Gonzalo <Gonzalo@ILoveRedHeads>
2017-12-18 19:05:14 +0100

    Commit para subir final

--- src/lovivido/LoVivido.java
+++ src/lovivido/LoVivido.java
@@ -10,7 +10,10 @@ package lovivido;
  */
 
 public class LoVivido {
-//Esta clase solo servira para llamar a los metodos mediante un objeto que creamos
+/**
+ * En la main solo llamaremos a los metodos mediante un objeto que creamos
+ * @param args 
+ */
 public static void main(String[] args) {
 CalcularTiempo obx = new CalcularTiempo();
 obx.recibirNombreEdad(obx.nombre, obx.edad);
